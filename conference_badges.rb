ad9031445abea3160c40ba62b98cad6e52d9c090
Ana Harris <vana1990@gmail.com>
2018-09-28 20:43:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |i|
+    badges << badge_maker(i)
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |item,index| 
+  room_assignments << "Hello, #{item}! You'll be assigned to room #{index + 1}!"
+  end
+room_assignments
+end
+
+def printer (attendees)
+  badges = batch_badge_creator(attendees)
+  room_assignments = assign_rooms(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+  room_assignments do |room|
+    puts room
+  end
+end
